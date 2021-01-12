f630d0d42453e1bca6ea2086bdb0f3acfc256963
chijey <jack.wu@things-matrix.com>
2021-01-12 16:01:33 +0800

    去掉配置

--- src/main/java/com/imooc/config/WebSocketConfig.java
+++ src/main/java/com/imooc/config/WebSocketConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-/**
- * Created by wwd
- */
 @Component
 public class WebSocketConfig {
 
